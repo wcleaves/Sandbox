eb53666f8c2150554d77fedbc4071407d70642cd
William <robertcleaves@ROBERTs-MacBook-Pro-3.local>
2014-05-13 11:00:54 -0400

    Added About page

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -1,4 +1,8 @@
 class PageController < ApplicationController
   def index
   end
+
+  def about
+  end
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root 'page#index'
+  get'/about'=>"page#about"
+  
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
